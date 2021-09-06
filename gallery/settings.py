c8b493a30c7ab6418d2538a7be763568098f68a2
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-06 17:39:53 +0300

    updates database pictures

--- gallery/settings.py
+++ gallery/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'picsgallery',
         'USER': 'moringa',
         'PASSWORD': 'kodhanjo',
@@ -122,4 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
