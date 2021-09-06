1b11e75b824f1b972666232912b11d0c6d2938da
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-06 12:39:50 +0300

    added getting days of the week url parten

--- photos/urls.py
+++ photos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     # url('^$',views.photos_of_day,name='photosToday'),
     url(r'^$',views.picture,name = 'picture'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.image,name ='image')
 ]
