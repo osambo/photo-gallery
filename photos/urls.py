b64996131a9a1245aa1cec581a494f9c59fb8ed4
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-07 01:49:58 +0300

    added changes in database

--- photos/urls.py
+++ photos/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     # url('^$',views.photos_of_day,name='photosToday'),
@@ -8,3 +10,6 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^image/(\d+)',views.image,name ='image')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
